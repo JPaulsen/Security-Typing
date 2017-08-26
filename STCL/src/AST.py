96052ea35366c777c2394e7320a2d085a2fb118a
Juan Pablo <juanpablo.paulsen@gmail.com>
2017-08-25 22:27:18 -0300

    refactor move types entities to Types.py

--- STCL/src/AST.py
+++ STCL/src/AST.py
@@ -1,4 +1,4 @@
-from sexpdata import *
+from Types import *
 
 
 class BoolLiteral:
@@ -132,56 +132,6 @@ class ApplyExpression:
 
     def __str__(self):
         return "(apply " + str(self.functionExpression) + " " + ", ".join(map(str, self.argumentExpressions)) + ")"
-
-
-class FunctionType:
-    def __init__(self, returnType, parameterTypes):
-        self.returnType = returnType
-        self.parameterTypes = parameterTypes
-
-    def __eq__(self, other):
-        if (other == None):
-            return False
-        parametersLength = len(self.parameterTypes)
-        ans = self.returnType == other.returnType and parametersLength == len(other.parameterTypes)
-        if (not ans):
-            return False
-        for i in range(parametersLength):
-            if (self.parameterTypes[i] != other.parameterTypes[i]):
-                return False
-        return True
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    def __str__(self):
-        return "function " + str(self.returnType) + " [" + ", ".join(map(str, self.parameterTypes)) + "]"
-
-
-def solveType(expr):
-    if (isinstance(expr, Symbol)):
-        expr = expr.value()
-        if (expr == "str"):
-            return str
-        elif (expr == "bool"):
-            return bool
-        elif (expr == "int"):
-            return int
-        elif (expr == "float"):
-            return float
-        else:
-            raise ValueError(expr + 'is not a valid type.')
-    try:
-        if expr[0].value() == "function":
-            returnType = solveType(expr[1])
-            parameterTypes = []
-            for parameter in expr[2].value():
-                parameterTypes.append(solveType(parameter))
-            return FunctionType(returnType, parameterTypes)
-        else:
-            raise ValueError(str(expr) + 'is not a valid type.')
-    except:
-        raise ValueError(str(expr) + 'is not a valid type.')
 
 
 class Environment:

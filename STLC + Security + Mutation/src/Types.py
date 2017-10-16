241563b94203b21d441903e92a76dcf397afc600
Juan Pablo Paulsen <juanpablo.paulsen@gmail.com>
2017-10-16 20:39:29 -0300

    Fix consistent types logic in STLC + Security + Mutation.

--- STLC + Security + Mutation/src/Types.py
+++ STLC + Security + Mutation/src/Types.py
@@ -3,24 +3,6 @@ class FunctionType:
         self.returnType = returnType
         self.parameterTypes = parameterTypes
 
-    def __eq__(self, other):
-        if other == None or not isinstance(other, FunctionType):
-            return False
-        parametersLength = len(self.parameterTypes)
-        ans = self.returnType.type == other.returnType.type and parametersLength == len(other.parameterTypes)
-        if not ans:
-            return False
-        for i in range(parametersLength):
-            if self.parameterTypes[i].type != other.parameterTypes[i].type:
-                return False
-        return True
-
-    def __le__(self, other):
-        return self == other
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
     def __str__(self):
         return "function " + str(self.returnType) + " [" + ", ".join(map(str, self.parameterTypes)) + "]"
 
@@ -29,21 +11,6 @@ class SecurityType:
     def __init__(self, type, securityLabel):
         self.type = type
         self.securityLabel = securityLabel
-
-    def __eq__(self, other):
-        if other == None or not isinstance(other, SecurityType):
-            return False
-        return self.type == other.type and self.securityLabel == other.securityLabel
-
-    def __lt__(self, other):
-        if other == None or not isinstance(other, SecurityType):
-            return False
-        return self.type == other.type and self.securityLabel < other.securityLabel
-
-    def __le__(self, other):
-        if other == None or not isinstance(other, SecurityType):
-            return False
-        return self.type == other.type and self.securityLabel <= other.securityLabel
 
     def __str__(self):
         return "(" + str(self.type) + ", " + str(self.securityLabel) + ")"
@@ -60,11 +27,6 @@ class SecurityLabel:
     def __init__(self, type):
         self.type = type
         self.value = SecurityLabel.lattice[type]
-
-    def __eq__(self, other):
-        if other == None or not isinstance(other, SecurityLabel):
-            return False
-        return self.value == other.value
 
     def __lt__(self, other):
         if other == None or not isinstance(other, SecurityLabel):
@@ -100,6 +62,7 @@ class SecurityLabel:
     @staticmethod
     def meetMultiple(securityLabels):
         return reduce(SecurityLabel.meet, securityLabels, SecurityLabel("t"))
+
 
 class RefType(SecurityType):
     def __init__(self, referencedSecurityType):
